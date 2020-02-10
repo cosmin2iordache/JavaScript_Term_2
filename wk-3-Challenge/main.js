6d4147dcc6bc02f3e7e9793ac0062de265343a38
cosmin2iordache <cosmin2iordache@gmail.com>
2020-02-10 23:52:00 +0000

    up

--- wk-3-Challenge/main.js
+++ wk-3-Challenge/main.js
@@ -1,12 +1,13 @@
 const btn = document.querySelector('button');
 
-function random(number) {
-  return Math.floor(Math.random() * (number+1));
+function random(cosmin) {
+  return Math.floor(Math.random() * (cosmin+1));
 }
 
 btn.onclick = function() {
-  const rndCol = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
-  document.body.style.backgroundColor = rndCol;
+  const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+  // document.body.style.backgroundColor = randomColor;
+  document.querySelector('.centre').style.backgroundColor = randomColor;
 }
 
 //How about using this to make a dark/light mode for your portfolio site? Other ideas you could explore with this?
