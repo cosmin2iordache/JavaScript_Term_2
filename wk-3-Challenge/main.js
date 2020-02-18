53db4ea4a0f158d358ad89290a65a1914065bb72
Cosmin <cosmin2iordache@users.noreply.github.com>
2020-02-18 10:33:38 +0000

    update

--- wk-3-Challenge/main.js
+++ wk-3-Challenge/main.js
@@ -1,10 +1,19 @@
-const btn = document.querySelector('button');
+// const btn = document.querySelector('button');
+// const btn = document.querySelector(".buttonClass");
+const btn = document.getElementsByClassName("buttonClass")[0];
 
-function random(cosmin) {
-  return Math.floor(Math.random() * (cosmin+1));
+let cosmin = document.querySelector("h1");
+cosmin.textContent = "let's modifyf"
+
+function random(number) {
+  return Math.floor(Math.random() * (number+1));
 }
 
-btn.onclick = function() {
+
+// btn.onclick = function() {
+
+function onClick() {
+
   const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
   // document.body.style.backgroundColor = randomColor;
   // document.querySelector('.centre').style.backgroundColor = randomColor;
@@ -17,9 +26,28 @@ btn.onclick = function() {
   //   element.style.backgroundColor = randomColor;
   // });
 
+
+
+  // console.log("hello");
+  display(); // call display function from this one
 }
+
+function display(){
+
+  console.log("asdaskdjaskldjaklsjd")
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 //How about using this to make a dark/light mode for your portfolio site? Other ideas you could explore with this?
 
-
-console.log("hello");
